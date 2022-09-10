a4c3f0daa7211a0f3495064244f42483c297d2e6
Kristian Gregersen <kristiantgregersen@gmail.com>
2022-09-11 00:21:23 +0200

    Update AddExerciseScreen.js

--- screens/AddExerciseScreen.js
+++ screens/AddExerciseScreen.js
@@ -4,9 +4,11 @@ import Banner from '../components/Banner/Banner';
 import { Feather } from '@expo/vector-icons';
 
 import colors from '../config/colors';
+import { useNavigation } from '@react-navigation/native';
 
 function AddExerciseScreen() {
-    let hasTemplates = false;
+    const navigation = useNavigation();
+    let hasTemplates = true;
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -14,11 +16,11 @@ function AddExerciseScreen() {
                 <Banner />
                 <>
                     <Text style={{ textTransform: 'uppercase', color: colors.white, fontSize: 28, fontWeight: "600" }}>opret workout</Text>
-                    <Text>Min bentræning 1</Text>
+                    <Text style={{ color: colors.white, fontWeight: '200', fontSize: 24 }}>Min bentræning 1</Text>
                 </>
             </View>
             {hasTemplates ?
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', top: -100 }}>
                     <Text style={{ fontStyle: 'italic', color: colors.green_primary }}>Ingen øvelser tilføjet</Text>
                 </View> :
                 <ScrollView>
